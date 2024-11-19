9945d80c976111a6c543e97c41fa792761eb5fea
Gabriel Moneta <gabrielpmoneta@gmail.com>
2024-11-19 18:21:44 -0300

    função darkmode e menu slide

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ function atualizarCarrossel() {
 }
 
 document.addEventListener('DOMContentLoaded', atualizarCarrossel);
+
+//Modo DarkMode e Menu Slide
+// Seleção dos elementos
+const darkModeButton = document.getElementById("darkmode");
+const menuButton = document.getElementById("menu-toggle");
+const slideMenu = document.getElementById("slide-menu");
+const body = document.body;
+
+// Carrega o estado inicial do modo escuro do localStorage
+const darkModeEnabled = localStorage.getItem("darkMode") === "enabled";
+if (darkModeEnabled) {
+  body.classList.add("darkmode");
+  darkModeButton.classList.add("dark");
+}
+
+// Alterna entre os modos claro e escuro
+darkModeButton.addEventListener("click", () => {
+  const isDarkMode = body.classList.toggle("darkmode");
+  darkModeButton.classList.toggle("dark");
+
+  // Salva o estado no localStorage
+  localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+});
+
+// Alternar menu slide
+menuButton.addEventListener("click", () => {
+  slideMenu.classList.toggle("open");
+});
